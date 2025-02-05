203c404a1d3c0c629dc3ea8ee95f8fe280b413a5
favour-ux124 <bakemyday529@gmail.com>
2025-02-04 17:21:09 -0800

    first correction

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// welcome
+var word = "Welcome to Bake My Day";
+window.alert(word);
+
+
+
+
+
 // Add event listener to navigation links
 document.querySelectorAll('nav a').forEach(link => {
-
+})
